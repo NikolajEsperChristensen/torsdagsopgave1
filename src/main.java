0a13f4ec7b3e4e98aaf5767c9d9f9ea1e07b58de
Niels Skytte <nsc@venzoanalytics.com>
2026-09-02 13:37:02 +0200

    final commit

---
+++ src/main.java
@@ -0,0 +1,175 @@
+import java.util.stream.DoubleStream;
+public class main {
+    public static void main() {
+
+        //opgave 1
+       /* double[] items = {25.50, 15.00, 30.00};
+        double sum = DoubleStream.of(items).sum();
+        System.out.println(sum);
+
+        double momsP = 1.25;
+        double sumWithTax = sum * momsP;
+        System.out.println(sumWithTax);
+
+        */
+        //opgave 2
+        /*double foodPrice = 450.0;
+        double drinkPrice = 120.0;
+        double tips = 50.0;
+        int people = 4;
+        double totalPrice = foodPrice + drinkPrice + tips;
+
+        double pricePPerson = totalPrice / people;
+        System.out.println(pricePPerson);
+
+         */
+        //opgave 3
+        /*double oPris = 300.0;
+        double rProcent = 20;
+        System.out.println(oPris);
+
+        double priceAfterDiscount = oPris * (1 - (rProcent /100));
+        System.out.println(priceAfterDiscount);
+
+        double tax = 25.0;
+        double pricWithTax = priceAfterDiscount * (1 + tax /100);
+        System.out.println(pricWithTax);
+
+         */
+        //ogpave 4
+        /*int guest = 15;
+        int cupcakes = 40;
+        int cupcakesPerPerson = cupcakes / guest;
+        int leftOverCupcakes = cupcakes % guest;
+        System.out.println(cupcakesPerPerson);
+        System.out.println(leftOverCupcakes);
+
+         */
+
+        //opagave 5
+        /*StringBuilder greating = new StringBuilder();
+
+        String name = "Anna";
+        int age = 25;
+        greating.append("Hello, ");
+        greating.append(name);
+        greating.append("!");
+        greating.append("You are");
+        greating.append(age);
+        greating.append("years old");
+        System.out.println(greating);
+
+         */
+
+        //opgave 6
+        /*int score = 100;
+        System.out.println(score);
+        score += 50;
+        System.out.println(score);
+        score += 25;
+        System.out.println(score);
+
+         */
+
+        //opgave 7
+        /*int health = 100;
+        System.out.println(health);
+        health -= 15;
+        System.out.println(health);
+        health -= 20;
+        System.out.println(health);
+
+         */
+
+        //opgave 8
+        /*double price = 50.0;
+        System.out.println(price);
+        price *= 2;
+        System.out.println(price);
+
+         */
+
+        //opgave 9
+        /*int counter = 10;
+        System.out.println(counter);
+        counter ++;
+        System.out.println(counter);
+        counter --;
+        System.out.println(counter);
+
+         */
+
+        //opgave 10
+        /*int number = 10;
+        number += 5;
+        System.out.println(number);
+        number -= 3;
+        System.out.println(number);
+        number ++;
+        System.out.println(number);
+        number --;
+        System.out.println(number);
+
+         */
+        /*String hpComputer = "Laptop";
+        double cPrice = 7999.99;
+        boolean itemsLeft = true;
+        int itemsLeft = 15;
+        double procentDiscount = 10.0;
+        String catagori = "Electronics"
+
+         */
+
+        //opgave 12
+        /*String studentName = "Anna";
+        int pointsFromTest = 85;
+        int pointsFromAssignment = 92;
+        double finalaverage = 88.5;
+        boolean passed = true;
+
+         */
+
+        //opgave 13
+        /*String guestname = "Peter Hansen";
+        int guestRoom = 204;
+        int nights = 3;
+        double pricePerNight = 899.0;
+        boolean breakfastIncluded = true;
+        double totalPrice = 2697.0;
+
+         */
+
+        //opgave 14
+        /*int stepsInDay = 8547;
+        int targetSteps = 10000;
+        double kilomiters = 6.2;
+        int kaloriesBurned = 420;
+        boolean targetComplete = false;
+
+         */
+
+        //opgave 15
+        String bookName = "harry potter";
+        String aurtherName = "jk rowling";
+        int isbnNumber = 14025;
+        int pagesInBook = 400;
+        boolean available = false;
+        String whoBorrowedBook = "Hans";
+        String bookReturnDay = " 5th of may";
+        int payIfLate = 100;
+        boolean isBookBestseller = false;
+        double booksRatings = 4,2;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
